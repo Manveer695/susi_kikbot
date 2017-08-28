dee370ed0baf960f03139d958d536de6ba93a484
Manveer695 <singhmanveer1995@gmail.com>
2017-08-28 23:57:47 +0530

    table

--- susi.js
+++ susi.js
@@ -42,9 +42,9 @@ const askSusi=function (query,cb) {
     else {
       cb('Oops, Looks like Susi is taking a break, She will be back soon')
     }
-  }).catch(e){
+  }).catch(function(e){
 
-  };
+  });
 }
 
 exports.ask=askSusi
