58f889a72c15815c9b4c316ff5cb0f933f8a4b3f
Manveer695 <singhmanveer1995@gmail.com>
2017-08-28 23:52:39 +0530

    table

--- susi.js
+++ susi.js
@@ -1,6 +1,6 @@
 const request=require('request')
 const askSusi=function (query,cb) {
-  request('http://api.asksusi.com/susi/chat.json?q='+encodeURI(query), function (error, response, body) {
+  request('http://api.susi.ai/susi/chat.json?q='+encodeURI(query), function (error, response, body) {
     if (!error && response.statusCode == 200) {
       var data = JSON.parse(body);
 		message = '';
@@ -42,7 +42,7 @@ const askSusi=function (query,cb) {
     else {
       cb('Oops, Looks like Susi is taking a break, She will be back soon')
     }
-  })
+  });
 }
 
 exports.ask=askSusi
